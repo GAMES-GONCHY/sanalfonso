cbb52dc06e1c324e78d908b7917241c39263bd2e
CIN2024 <cinthia.quispe.795@gmail.com>
2025-01-10 12:52:22 -0400

    creacion del contolador api.php y sus lineas de codigo para hacer la pureba si funciona la api

---
+++ codeigniter/application/controllers/Api.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use chriskacerguis\RestServer\RestController;
+
+class Api extends RestController {
+
+   
+    public function prueba_get() {
+        $this->response([
+            'status' => true,
+            'message' => 'La API REST está funcionando correctamente'
+        ], 200);
+    }
+}
